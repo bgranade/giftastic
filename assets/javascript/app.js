919a68d344743fadd40092ce8b2c2f1f831d8de1
bethgranade <bgranado488@gmail.com>
2019-11-02 10:35:45 -0500

    header/search css and intiial js code

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    //Array for searched topics to be added
+    var topics = [];
+    
+        //Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
+      //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
+         function display_topics() {
+    
+        var x = $(this).data("search");
+        console.log(x);
+    
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=TevVqa61oCyK681cGUE5NbMihi06pAf6&limit=25&rating=R";
+    
+        console.log(queryURL);
+
+        
